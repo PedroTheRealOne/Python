2f0735c9fc76d382fc36a9269c714d51207d6983
Pedro_myeong <pedrotherealone1@gmail.com>
2017-11-01 23:44:13 -0200

    Waifu_bot

--- Waifu_Bot/WaifuBot.py
+++ Waifu_Bot/WaifuBot.py
@@ -5,17 +5,23 @@ from chatterbot import ChatBot
 
 bot = ChatBot('Waifu')
 
-chatI = ['ohayo','ohayo senpai','How r u?','Im fine','Senpai, What are you doing?']
+chatI = ['ohayo','ohayo senpai','Supp','How rude','How are you?','Im fine','Senpai, What are you doing?', 'I love you senpai']
 
 chatT = ['Im just waiting you', 'Thanks', 'How was your day, Senpai?']
+
+chatQ = ['What do you like to eat?','What do you like to do?']
+
+chatR = ['Senapi, untie me please.']
 
 chatDump = ['You are scarring me']
 
 bot.set_trainer(ListTrainer)
 
 bot.train(chatI)
-bot.train(chatDump)
 bot.train(chatT)
+bot.train(chatQ)
+bot.train(chatR)
+bot.train(chatDump)
 
 while True:
     question = input('Daddy: ')
