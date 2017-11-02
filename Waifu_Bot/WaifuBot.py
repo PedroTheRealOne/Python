0252144212c53ab01ea61669ea57f18bb8d24ac9
Pedro_myeong <pedrotherealone1@gmail.com>
2017-11-01 23:06:23 -0200

    Bot_Waifu
    Trying to figure out, how this work

--- Waifu_Bot/WaifuBot.py
+++ Waifu_Bot/WaifuBot.py
@@ -5,15 +5,24 @@ from chatterbot import ChatBot
 
 bot = ChatBot('Waifu')
 
-chat = ['ohayo','ohayo senpai','How r u?','Im fine']
+chatI = ['ohayo','ohayo senpai','How r u?','Im fine','Senpai, What are you doing?']
+
+chatT = ['Im just waiting you', 'Thanks']
+
+chatDump = ['You are scarring me']
 
 bot.set_trainer(ListTrainer)
 
-bot.train(chat)
+bot.train(chatI)
+bot.train(chatDump)
+bot.train(chatT)
 
 while True:
     question = input('Daddy: ')
 
     response = bot.get_response(question)
-
-    print('Waifu: ', response)
+    
+    if float(response.confidence) > 0.5:
+        print('Waifu: ', response)
+    else:
+        print('Waifu: ', response)
